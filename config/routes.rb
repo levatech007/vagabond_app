9ae599d27db926b4e7aa4e5061c1200bba6509d7
Eva <evaliisagalen@gmail.com>
2018-01-12 08:13:32 -0800

    updated users routes to resources instead on individually listing them

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "site#index"
 
-  get "users/new", to: "users#new"
+  resources :users, only: [:new, :create, :show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
