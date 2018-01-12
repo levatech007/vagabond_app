e7d19e0ebb9d3c6f1e62c7b17d14dd8a4409727d
Xianyi555 <xianyi555@gmail.com>
2018-01-12 14:15:45 -0800

    working for show post to user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: "site#index"
 
-  resources :users, only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :users, only: [:new, :create, :show, :edit, :update, :destroy] do
+  	resources :posts, only: [:show]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/login", to: "sessions#new"
   get '/logout', to: 'sessions#destroy'
