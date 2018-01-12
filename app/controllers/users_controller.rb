6e9c05705d8dce805ab56e7fc5d4db79309380d7
Eva <evaliisagalen@gmail.com>
2018-01-12 09:09:31 -0800

    added working signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,24 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-  end 
+  end
 
   def new
     @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+      redirect_to user_path(@user)
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :user_city, :password)
   end
 
 end
