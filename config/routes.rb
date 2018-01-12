a41a9d7e6d1095f0d06bfab36e18f23edb33caf9
Xianyi555 <xianyi555@gmail.com>
2018-01-11 23:22:28 -0800

    completed Posts Show and added a link for each post

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: "site#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/posts", to: "posts#index", as: "posts"
+  get "/posts/:id", to: "posts#show", as: "post"
 end
