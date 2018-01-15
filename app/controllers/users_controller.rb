09de210bc7e02f67e370c9e8a14e2a0482b284fb
Xianyi555 <xianyi555@gmail.com>
2018-01-15 03:03:35 -0800

    completed PaperClip feature for Update Users profile picture

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
+      login(@user)
       redirect_to user_path(@user)
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
 
   has_many :posts
   has_secure_password
-
+  has_attached_file :avatar, styles: { small: "64x64", med: "100x100", large: "300x300" }
+  validates_attachment :avatar, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
     @user ? @user.authenticate(params[:password]) : false
