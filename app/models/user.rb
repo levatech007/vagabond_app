567d3857e1f9263b7bab9f2b7266d5b8d0782864
Eva <evaliisagalen@gmail.com>
2018-01-15 17:44:33 -0800

    added functions to check if user is logged in

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_secure_password
 
-  validates :first_name, presence: true, length:   { maximum: 255 }
-  validates :last_name, presence: true, length:   { maximum: 255 }
+  validates :first_name, presence: true, length: { maximum: 255 }
+  validates :last_name, presence: true, length: { maximum: 255 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true, length: { maximum: 255 }
 
