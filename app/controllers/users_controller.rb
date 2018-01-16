ad5386e6fad6cd27cba4e46580b1a07ddc23258f
Eva <evaliisagalen@gmail.com>
2018-01-15 18:00:14 -0800

    created cities controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  #before_action :require_login, only: [:show]
+
   def index
     @users = User.all
   end
@@ -39,8 +41,8 @@ class UsersController < ApplicationController
       redirect_to root_path
     end
 
-
   private
+
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :user_city, :password)
   end
