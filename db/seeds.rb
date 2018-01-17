78c550d6c4b2d98eb31b777931a41191c1aa5f8c
Xianyi555 <xianyi555@gmail.com>
2018-01-16 23:33:26 -0800

    s

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ Post.destroy_all
 City.destroy_all
 
 
-users = User.create(users_data)
+
 
 city_names = ["San Francisco", "Los Angeles", "New York", "Miami", "Chicago"]
 cities_data = []
@@ -32,7 +32,6 @@ posts_data = []
   posts_data << {
     title: "Travel review",
     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nunc nisl, lacinia sit amet fringilla ac, posuere quis dui. Sed vel fermentum orci. Aenean a turpis quam. Ut turpis magna, lobortis a purus in, cursus tempus elit. Sed dignissim laoreet sagittis. Quisque nec ornare neque. Sed sed metus sollicitudin elit congue placerat et vel enim. In vitae arcu a purus hendrerit condimentu",
-    user: users.sample,
     city: cities.sample
   }
 end
